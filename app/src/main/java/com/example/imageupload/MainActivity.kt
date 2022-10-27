4dbdc0a6c6f60b543d593a5e117ed106fcd85390
Gian Felipe da Silva <gian.silva@cialne.com.br>
2022-10-27 13:09:02 -0300

    Adiciona função de inserir imagem pela camera

--- app/src/main/java/com/example/imageupload/MainActivity.kt
+++ app/src/main/java/com/example/imageupload/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.imageupload
 
+import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -10,20 +12,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.RecyclerView
+import androidx.core.content.FileProvider
 import com.example.imageupload.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var dialog : AlertDialog
+    private var uriImagem :Uri? = null
+    private val contract = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+        binding.imageView.setImageURI(null)
+        binding.imageView.setImageURI(uriImagem)
+    }
+
 
     companion object {
-        private val PERMISSAO_GALERIA = android.Manifest.permission.READ_EXTERNAL_STORAGE
+        private const val PERMISSAO_GALERIA = android.Manifest.permission.READ_EXTERNAL_STORAGE
     }
 
     private val requestGaleria =
@@ -32,9 +41,7 @@ class MainActivity : AppCompatActivity() {
             resultGaleria.launch(
                 Intent(
                     Intent.ACTION_PICK,
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                )
-            )
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI))
         } else {
             showDialogPermissao()
         }
@@ -51,8 +58,7 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     val source = ImageDecoder.createSource(
                         this.contentResolver,
-                        result.data?.data!!
-                    )
+                        result.data?.data!!)
                     ImageDecoder.decodeBitmap(source)
                 }
                 binding.imageView.setImageBitmap(bitmap)
@@ -65,6 +71,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.Button.setOnClickListener { verificarPermissaoGaleria() }
         binding.ButtonNav.setOnClickListener { navigateToRecycler() }
+        uriImagem = obterImagemCamera()
+        binding.ButtonCamera.setOnClickListener { contract.launch(uriImagem) }
     }
 
     private fun verificarPermissaoGaleria() {
@@ -76,18 +84,13 @@ class MainActivity : AppCompatActivity() {
                     Intent(
                         Intent.ACTION_PICK,
                         MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                    )
-                )
+                    ))
             }
 
             shouldShowRequestPermissionRationale(PERMISSAO_GALERIA) -> showDialogPermissao()
 
              else -> requestGaleria.launch(PERMISSAO_GALERIA)
         }
-    }
-
-    private fun navigateToRecycler() {
-        startActivity(Intent(this, RecyclerActivity::class.java))
     }
 
     private fun showDialogPermissao() {
@@ -99,8 +102,7 @@ class MainActivity : AppCompatActivity() {
             }
             .setPositiveButton("Sim") {_, _ ->
                 val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                Uri.fromParts("package", packageName, null)
-                )
+                Uri.fromParts("package", packageName, null))
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 startActivity(intent)
                 dialog.dismiss()
@@ -111,4 +113,37 @@ class MainActivity : AppCompatActivity() {
 
     private fun verificaPermissao(permissao: String) =
         ContextCompat.checkSelfPermission(this, permissao) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun obterImagemCamera() : Uri? {
+        val image = File(applicationContext.filesDir, "my_images.png")
+        return FileProvider.getUriForFile(applicationContext,
+            "com.example.imageupload",
+            image
+        )
+    }
+
+//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        val contentValues = ContentValues()
+//        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+//
+//        val resolver = contentResolver
+//
+//        uriImagem = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+//
+//        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+//                or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+//
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, uriImagem)
+//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+//            if(result.resultCode == Activity.RESULT_OK) {
+//                val data: Intent? = result.data
+//            }
+//        }
+
+
+
+    private fun navigateToRecycler() {
+        startActivity(Intent(this, RecyclerActivity::class.java))
+    }
 }
